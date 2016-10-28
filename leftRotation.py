06888381ebbd4ee4b7537e8248e8eca891d00a22
damedebugger <anja.kefala@gmail.com>
2016-10-28 15:28:08 -0400

    Added a hackerrank puzzle from the Cracking the Coding Interview module. No code yet.

---
+++ leftRotation.py
@@ -0,0 +1,14 @@
+# Left rotation - shifts each of the array's elements 1 unit to the left
+# E.g. 2 left rotations are performed on [1, 2, 3, 4, 5]
+# Results in an array [3, 4, 5, 1, 2
+# Given an array of n integers and a number d, perform d left rotations on the array
+# Then print the updated array as a single line of space-separated integers
+
+# Input Format
+
+# The first line contains two space-separated integers denoting the respective values of (the number of integers) and (the number of left rotations you must perform).
+# The second line contains space-separated integers describing the respective elements of the array's initial state.
+
+# Constraints 1 <= n <= 10^5
+# 1 <= d <= n
+# 1 <= ai <= 10^6
