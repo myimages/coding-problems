dfedbdbd9529ac083332d70b5b06e6e75ca8b83f
damedebugger <anja.kefala@gmail.com>
2016-10-29 13:26:53 -0400

    Added a function which processes a sample file. Assumes the array is on a single line.

--- leftRotation.py
+++ leftRotation.py
@@ -1,2 +1,25 @@
+def processFile(filename):
+    with open(filename, 'r') as f:
+        # Input:
+        # numberOfIntegers numberofLeftRotations
+        # Space Separated Integers
+        n, d = f.readline().split(" ")
+        n = int(n)
+        d = int(d)
+        # Convert the space separated string of numbers
+        # into a list of integers
+        arr = f.readline().split(" ")
+        i = 0
+        for num in arr:
+            arr[i] = int(num)
+            i += 1
+        return d, arr
 
 
+def leftRotation (arr, d):
+    # Perform d left rotations on array
+    print("")
+
+d, arr = processFile("leftRotation.txt")
+print(d)
+print(arr)
