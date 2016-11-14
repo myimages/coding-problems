a008534e95bbc290a9150555fe9f711e007ae74e
damedebugger <anja.kefala@gmail.com>
2016-11-14 15:10:36 -0500

    Making plans.

---
+++ makingAnagrams.py
@@ -0,0 +1,25 @@
+def letterFrequency(s):
+    """"
+    Given a string, s, returns a dictionary that asseses the letter frequencies in that string in a case-insensitive manner.
+
+    Parameters
+    s: String, alphabet letters only
+
+    Returns
+    dictS: Dictionary which records all of the letters contained in s, with their frequencies of occurrences in a case-insensitive manner
+    """
+
+def makingAnagrams (a, b):
+    """
+    Given two strings, a and b, determines the minimum number of character deletions required to make a and b anagrams. Any characters can be deleted from either of the strings.
+
+    Parameters
+    a: String, Consists of lowercase English alphabetic letters
+    b: String, Consists of lowercase English alphabetic letters
+
+    Returns
+    n: Integer, The number of characters you must delete to make the two strings anagrams of each other
+    """
+
+    dictA = letterFrequency(a)
+    dictB = letterFrequency(b)
