ab29a96f38236faa66c45ec54ec653e5f036c5d8
damedebugger <anja.kefala@gmail.com>
2016-11-18 13:55:07 -0500

    Should I make it a hash table?

--- ransomnote.py
+++ ransomnote.py
@@ -1,20 +1,22 @@
-def key_to_index(key, M):
-    """
-    Converts a string key into an array index for an array of size M.
+class hash_table:
 
-    PARAMETERS
-    key: String, alphabet only, case-sensitive, no space
-    M: Int
-    RETURNS
-    hash_index: Int, an index into the hash table based on the key
-    """
-    hash_index = 0
-    # 31 can be changed for another small, prime number
-    # Formula for conversion of a string of characters into an index into a hash table.
-    for c in key:
-        hash_index = (31 * hash_index + ord(c)) % M
+    def key_to_index(key, M):
+        """
+        Converts a string key into an array index for an array of size M.
 
-    return(hash_index)
+        PARAMETERS
+        key: String, alphabet only, case-sensitive, no space
+        M: Int
+        RETURNS
+        hash_index: Int, an index into the hash table based on the key
+        """
+        hash_index = 0
+        # 31 can be changed for another small, prime number
+        # Formula for conversion of a string of characters into an index into a hash table.
+        for c in key:
+            hash_index = (31 * hash_index + ord(c)) % M
+
+        return(hash_index)
 
 #def ransom_note(magazine, ransom):
 
