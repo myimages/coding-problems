5187606743ee9aaf45c30919606de8140ddeaf82
damedebugger <anja.kefala@gmail.com>
2016-11-23 20:47:02 -0500

    Added comments and assertions/error handling.

--- ransomnote.py
+++ ransomnote.py
@@ -1,5 +1,11 @@
 class hash_table:
 
+    def __init__(self):
+        """"
+        Initializes a hash table. Size of hash table (currently 1009) must be a prime number.
+        """
+        self.table = [None] * 1009
+
     def key_to_index(self, key, M):
         """
         Converts a string key into an array index for an array of size M.
@@ -18,20 +24,34 @@ class hash_table:
         for c in key:
             hash_index = hash_index*i + ord(c)
             i += 1
-
+        assert hash_index >= 1
         return(hash_index % M)
 
     def add_word(self, word):
+        """
+        Adds a word to the hash table
+        PARAMETERS
+        word: String, alphabet only, case-sensitive, no space
+        """
         hash_index = self.key_to_index(word, len(self.table))
+        assert hash_index <= len(self.table)
+        # If there currently isn't a word located at that hash index, create a new list to place in that index.
         if self.table[hash_index] is None:
             l = []
             l.append(word)
             self.table[hash_index] = l
         else:
+            assert type(self.table[hash_index]) is list
             self.table[hash_index].append(word)
 
     def add_sentence(self, sentence):
-        words = sentence.split(" ")
+        """
+        Takes a sentence either in the form of a list or string and ensures each word (separated by spaces) is added to the hash table.
+        """
+        if (type(sentence) is str):
+            words = sentence.split(" ")
+        else:
+            words = sentence
         for word in words:
             self.add_word(word)
 
@@ -63,8 +83,6 @@ class hash_table:
     def print_table(self):
         print(self.table)
 
-    def __init__(self):
-        self.table = [None] * 1009
 
 def ransom_note(magazine, ransom):
     h_table = hash_table()
