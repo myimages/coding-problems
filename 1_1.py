d434355c846329012d04b3baa67b932f9afb7881
Anja Kefala <anja.kefala@gmail.com>
2017-02-14 10:18:35 -0500

    Decided to merge the function hex to binary with hex to 64.

--- 1_1.py
+++ 1_1.py
@@ -1,15 +1,13 @@
-def hex_to_binary(HexStr):
-    # Hex to Binary using built-in python functions
-    BinaryLit  = bin(int(HexStr, 16))
-    return BinaryLit
-
 def hex_to_64 (HexStr):
     """
     Convert a hex string to a base64 string.
-    Input: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
-    Output: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+    Practice Input: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+    Practice Output: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
     """
+
+    # Converts hex string to binary literal
+    BinaryLit  = bin(int(HexStr, 16))
     print(hex_to_binary(HexStr))
     #return Base64String
 
-hex_to_64("ff")
+hex_to_64("0xff")
