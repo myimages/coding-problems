e3f1f3cacde6d442dea06a5081da1cfdb15085c8
Anja Kefala <anja.kefala@gmail.com>
2017-02-15 17:11:23 -0500

    Figured out a way to store the hex value as a variable.

--- 1_2.py
+++ 1_2.py
@@ -1,8 +1,14 @@
 import base64
 
-bin1= int("1c0111001f010100061a024b53535009181c", 16)
-bin2 = int("686974207468652062756c6c277320657965", 16)
+def fixed_XOR(Hex1, Hex2):
+    Bin1= int(Hex1, 16)
+    Bin2 = int(Hex2, 16)
 
-answer = bin1^bin2
+    BinXOR= Bin1^Bin2
+    HexXOR = hex(BinXOR)
+    print(HexXOR)
 
-print("%x" % answer)
+Hex1 = "1c0111001f010100061a024b53535009181c"
+Hex2 = "686974207468652062756c6c277320657965"
+
+fixed_XOR(Hex1, Hex2)
