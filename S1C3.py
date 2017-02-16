7369d3e5e5050211d2411ea7a8d59cba3d56c4b0
Anja Kefala <anja.kefala@gmail.com>
2017-02-16 13:42:20 -0500

    Iterate through the lowercase letters in the alphabet, convert them to hex and XOR with the message. TODO: Convert the XORd messages back to an English plaintext form.

--- S1C3.py
+++ S1C3.py
@@ -1,6 +1,14 @@
 import S1C1
 import S1C2
+from string import ascii_lowercase
 
-def single_byte_XOR_cypher(cypher):
+def single_byte_XOR_cypher(ciphertext):
+    """
+    Decrypts a hex encoded string that was encrypted by being XORd against a single character.
+    """
+    for c in ascii_lowercase:
+        c_hex = hex(ord(c))
+        S1C2.fixed_XOR(ciphertext, c_hex)
 
+single_byte_XOR_cypher("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
 
