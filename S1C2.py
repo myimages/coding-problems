ec839fb4eae6f03f60c147750a4394efcb7820a0
Anja Kefala <anja.kefala@gmail.com>
2017-02-16 13:23:11 -0500

    Changed naming conventions to make importing easier.

--- S1C2.py
+++ S1C2.py
@@ -6,7 +6,10 @@ def fixed_XOR(Hex1, Hex2):
     HexXOR = hex(BinXOR)
     return HexXOR
 
-Hex1 = "1c0111001f010100061a024b53535009181c"
-Hex2 = "686974207468652062756c6c277320657965"
+"""
+Testing
+"""
+#Hex1 = "1c0111001f010100061a024b53535009181c"
+#Hex2 = "686974207468652062756c6c277320657965"
 
-print(fixed_XOR(Hex1, Hex2))
+#print(fixed_XOR(Hex1, Hex2))
---
+++ S1C3.py
@@ -0,0 +1,6 @@
+import S1C1
+import S1C2
+
+def single_byte_XOR_cypher(cypher):
+
+
