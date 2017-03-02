f60efda568300b3f82924c1c8b2c673f75bc6df2
Anja Kefala <anja.kefala@gmail.com>
2017-03-02 09:58:24 -0500

    Used vcr to write a broken test, and now let's get it to work!

--- crossword_scraper.py
+++ crossword_scraper.py
@@ -11,15 +11,13 @@ def crawl_website(url):
     Reads a page from a url and then prints it to a file
     GOAL: Will read a series of pages defined by another variable. For now, let's just read one page.
     """
-    CrosswordPage = requests.get(url)
-    tree = html.fromstring(CrosswordPage.content)
+    Page = requests.get(url)
+    tree = html.fromstring(Page.content)
     # We can reasonably assume that this will work okay for 1 page.
     # We will get it working for 1 page and then expand to multiple pages with scrapy?
-    # This will return a 1 if crawling was successful and a 0 if crawling was not successful eventually
-    print(tree)
-    return 0
+    return tree
 
-def scrape_website(f):
+def scrape_nytcrossword(f):
     """
     Process the data from a website offline
     """
--- test_crossword_scraper.py
+++ test_crossword_scraper.py
@@ -4,6 +4,10 @@
 # - download the fetch once - and save it to a file and then test to your hearts desire
 # https://github.com/kevin1024/vcrpy
 import crossword_scraper
-import responses
 import vcr
 
+# Let's get this test to work.
+@vcr.use_cassette("fixtures/vcr_cassettes/nytcrossword.yaml")
+def test_crawler_on_nytcrossword():
+    response = crossword_scraper.crawl_website("http://www.nytcrossword.com/")
+    assert "<html>" in response
