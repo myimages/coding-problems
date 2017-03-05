ecfc7e94c7d686b5f698579dbf6ce720e26695e7
Anja Kefala <anja.kefala@gmail.com>
2017-03-05 14:20:58 -0500

    Work in progress....

--- crossword_scraper.py
+++ crossword_scraper.py
@@ -2,8 +2,13 @@
 Data from: http://www.nytcrossword.com/
 Scrapes the site for crossword puzzles.
 """
-from lxml import html
+# Learn about virtualenv
+# Switch to lxml
 import requests
+from bs4 import BeautifulSoup
+# lxml - wrapper around beautifulsoup
+# You have to instantiate the etree - models the DOM
+# That model allows you to traverse the DOM
 
 # CURRENTLY WORKING ON
 def crawl_website(url):
@@ -11,14 +16,17 @@ def crawl_website(url):
     Reads a page from a url and then prints it to a file
     GOAL: Will read a series of pages defined by another variable. For now, let's just read one page.
     """
-    Page = requests.get(url)
-    tree = html.fromstring(Page.content)
+    Response = requests.get(url)
+    PageContent = Response.text
+    Soup = BeautifulSoup(PageContent, "html.parser")
     # We can reasonably assume that this will work okay for 1 page.
     # We will get it working for 1 page and then expand to multiple pages with scrapy?
-    return tree
+    # You're getting bytes and need to
+    return Soup
 
 def scrape_nytcrossword(f):
     """
     Process the data from a website offline
     """
     return 0
+
--- test_crossword_scraper.py
+++ test_crossword_scraper.py
@@ -6,8 +6,11 @@
 import crossword_scraper
 import vcr
 
+
 # Let's get this test to work.
 @vcr.use_cassette("fixtures/vcr_cassettes/nytcrossword.yaml")
 def test_crawler_on_nytcrossword():
     response = crossword_scraper.crawl_website("http://www.nytcrossword.com/")
-    assert "<html>" in response
+    print(response)
+
+test_crawler_on_nytcrossword()
