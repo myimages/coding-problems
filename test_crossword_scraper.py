95cd5427f9788b2a8fe4ec5becafc31f306fb132
Anja Kefala <anja.kefala@gmail.com>
2017-03-07 14:00:46 -0500

    Removed unecessary notes to self.

--- test_crossword_scraper.py
+++ test_crossword_scraper.py
@@ -1,8 +1,3 @@
-# Testing fetch
-
-# Testing parse
-# - download the fetch once - and save it to a file and then test to your hearts desire
-# https://github.com/kevin1024/vcrpy
 import crossword_scraper
 import vcr
 from lxml import html, etree
@@ -15,6 +10,7 @@ def test_crawler_on_nytcrossword():
     """
     Tests our web crawler on the nytcrossword front page.
     """
+
     response = crossword_scraper.crawl_website("http://www.nytcrossword.com/")
     # Does my crawler create an element tree?
     assert etree.iselement(response)
@@ -22,17 +18,23 @@ def test_crawler_on_nytcrossword():
     # Does my element tree contain the information I need?
     assert "Across" in response.xpath("//text ()")
 
-def test_crawler_exception_handling():
+def test_crawler_error_handling():
     """
     Tests that our web crawler exception handles invalid urls.
     """
-    crossword_scraper.crawl_website("nytcrossword", error_file = "errors_tmp.txt")
-    f = open("errors_tmp.txt", "r")
-    errors = f.read()
+
+    crossword_scraper.crawl_website("nytcrossword", error_file_loc = "errors_tmp.txt")
+    error_f = open("errors_tmp.txt", "r")
+    errors = error_f.read()
     assert "nytcrossword" in errors
-    f.close()
+    error_f.close()
     os.remove("errors_tmp.txt")
 
+@vcr.use_cassette("fixtures/vcr_cassettes/nytcrossword_invalid.yaml")
+def test_scraper_on_invalid_page():
+    response = crossword_scraper.crawl_website("http://www.nytcrossword.com/page/1")
+    scrape = crossword_scraper.scrape_nytcrossword(response)
+    assert scrape == None
 
 def test_scraper_on_nytcrossword():
     # How would I make this independent? I'd have to read the file and turn it into an element tree. The thing I dreaded doing. Sigh.
