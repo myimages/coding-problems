e485b1b1ccb093353d17f58333040c3f28ee5c1a
Anja Kefala <anja.kefala@gmail.com>
2017-03-07 11:11:37 -0500

    Changed naming style to follow Google's naming style.

--- crossword_scraper.py
+++ crossword_scraper.py
@@ -5,64 +5,42 @@ Scrapes the site for crossword puzzles.
 # We're going to learn about these
 import requests
 from lxml import html, etree
-import re
-
-# BEGIN Code copied from the Django project
-def is_valid_url(Url):
-    """
-    Checks that a url is a valid url
-    PARAMETERS
-    Url: String
-    RETURNS
-    Boolean: True if Url is a valid url, False otherwise.
-    """
-    Regex = re.compile(
-    # http:// or https://
-        r'^https?://'
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domain...
-        r'localhost|'  # localhost...
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-        r'(?::\d+)?'  # optional port
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-    return Url is not None and Regex.search(Url)
-# END code copied from the Django project
 
 
-def crawl_website(Url):
+def crawl_website(url):
     """
     Reads one page from a url and then returns the DOMTree
     PARAMETERS
-    Url: String, a url representing a website that we wish to crawl
+    url: String, a url representing a website that we wish to crawl
     RETURNS
     DOMTree: An Element Tree created using the html module from the lxml library
     """
-    assert is_valid_url(Url)
-    Page = requests.get(Url)
-    DOMTree = html.fromstring(Page.content)
-    return DOMTree
+    # Need to add some error handling here. I don't want it to crash if the url is not valid
+    page = requests.get(url)
+    DOM_tree = html.fromstring(page.content)
+    return DOM_tree
 
-def etree_to_file(DOMTree):
+def etree_to_file(DOM_tree):
     return
 
-def scrape_nytcrossword(DOMTree):
+def scrape_nytcrossword(DOM_tree):
     """
     Process the data from the nyt website for clue:answer pairs
     """
 
     # We can use this to read the crossword solutions
-    DOMList = DOMTree.xpath("//text ()")
-    iAcross = [i for i, j in enumerate(DOMList) if j == "Across"]
-    iDown = [i for i, j in enumerate(DOMList) if j == "Down"]
-    Clues = []
-    for element in DOMList[iAcross[1]+1:iDown[1]]:
+    DOM_list = DOM_tree.xpath("//text ()")
+    i_across = [i for i, j in enumerate(DOM_list) if j == "Across"]
+    i_down = [i for i, j in enumerate(DOM_list) if j == "Down"]
+    clues = []
+    for element in DOM_list[i_across[1]+1:i_down[1]]:
         element = element.strip()
         if element:
-            Clues.append(element)
-    for element in DOMList[iDown[1]:]:
+            clues.append(element)
+    for element in DOM_list[i_down[1]:]:
         element = element.strip()
         if element:
-            Clues.append(element)
+            clues.append(element)
         else:
             break
-    print(Clues)
 
--- test_crossword_scraper.py
+++ test_crossword_scraper.py
@@ -8,15 +8,15 @@ import vcr
 from lxml import html, etree
 
 @vcr.use_cassette("fixtures/vcr_cassettes/nytcrossword.yaml")
-def test_crawler_works_on_nytcrossword():
+def test_crawler_on_nytcrossword():
     # Tests if crossword_scraper's crawl has returned an element tree.
     # TODO: Test for contents of element tree.
     response = crossword_scraper.crawl_website("http://www.nytcrossword.com/")
+    # Does my crawler create an element tree?
     assert etree.iselement(response)
     assert len(response) > 0
-    # Extracts the text content of a tree into a list.
+    # Does my element tree contain the information I need?
     assert "Across" in response.xpath("//text ()")
-    crossword_scraper.scrape_nytcrossword(response)
 
 def test_scraper_on_nytcrossword():
     # How would I make this independent? I'd have to read the file and turn it into an element tree. The thing I dreaded doing. Sigh.
