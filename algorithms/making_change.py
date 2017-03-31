8914072c18c4efafd846602a5cab1711575abcb0
Anja Kefala <anja.kefala@gmail.com>
2017-03-31 15:27:28 -0400

    Add method documentation.

--- algorithms/making_change.py
+++ algorithms/making_change.py
@@ -1,1 +1,19 @@
-def making_change():
+def making_change(value):
+    """
+    Evaluates how a given amount of money can be made with the fewest number of Canadian coins.
+    Args:
+        value(:float): The amount of money we want change for.
+    Returns:
+        change(:dictionary): The number of each coin which results in the fewest number of total coins being used to equal the value.
+    """
+    pass
+
+def total_monetary_value(coins):
+    """
+    Evaluates the total monetary value of a collection of coins.
+    Args:
+        coins(:dictionary): A dictionary which has a coin's value as a key followed by the total number of those coins of that value.
+    Returns:
+        value(:float): The total monetary coin value of the coin collection.
+    """
+    pass
